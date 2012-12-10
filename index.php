0b9bf578e98f2b4ee6ea6c25fa55b6758ed35052
Matthew Reidsma <reidsmam@gvsu.edu>
2012-12-10 07:55:21 -0500

    Too many spaces. Oops.

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ function new_get_file_contents($url) {
 	curl_setopt ($ch, CURLOPT_URL, $url);
 	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-	$file_contents = curl_ e x e c($ch); // take out the spaces of curl statement!!
+	$file_contents = curl_exec($ch); 
 	curl_close($ch);
 	return $file_contents;
 }
