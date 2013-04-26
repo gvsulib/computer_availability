cd2911937792d32272351c719cf669a4e5199b8a
Matthew Reidsma <matthew@reidsrow.com>
2013-04-26 12:32:34 -0400

    Fixed zumberge status for all screens

--- index.php
+++ index.php
@@ -642,7 +642,7 @@ if (!empty($_GET['x'])) {
 					} else {
 						echo '<div class="span1" style="margin-top:1em;"><h4 class="banner_title"><a href="?library=zumberge">Zumberge Library</h4></a></div>';
 						echo '<div class="line">';
-						printCompAvail("Zumberge", $building_results);
+								echo '<div class="span1 unit"><p><strong>Zumberge Library is closed</strong>. Come see our temporary location in the Kirkhof Center&#8217;s Thornapple Room!</p><p>Don&#8217;t worry, we&#8217;re building a bigger, better library for you and will open up in June.</p></div>';
 						echo '</div>';				
 						echo '<div class="line"><div class="span2 unit left"><h4 class="banner_title"><a href="?library=steelcase">Steelcase Library</a></h4>';
 						printCompAvail("Steelcase", $building_results);
